8a0d1334c62a69787651d210cb555d04212d7179
ItsEthanH <89361954+ItsEthanH@users.noreply.github.com>
2023-12-30 01:06:41 +0000

    deploy!

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,3 +2,4 @@
 // All JS files are sent to public via esbuild, as if it was an eleventy passthrough. This can be configured like any other bundler
 import "./dark";
 import "./nav";
+import "./shopify";
